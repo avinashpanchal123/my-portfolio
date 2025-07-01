06d08460d56b8448e28c12f35a196bacdff914ac
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2025-07-01 21:12:37 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,13 +19,13 @@ const About = () => {
             </p>
              <Resume/>
             <div className="mt-12 flex justify-center text-center">
-                <div>
+{/*                 <div>
                     <h3 className="text-2xl font-bold text-transparent bg-clip-text 
                 bg-gradient-to-r from-green-400 to-blue-500">
                         3+
                     </h3>
                     <p>Years Experience</p>
-                </div>
+                </div> */}
                
             </div>
           </div>
